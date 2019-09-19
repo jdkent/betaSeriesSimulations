2f66c2da6eadc0deb48b25aad87b6ee596c53c3d
James Kent <james-kent@uiowa.edu>
2019-09-19 08:47:36 +0000

    add simple workflow

--- beta_sim/tests/test_workflow.py
+++ beta_sim/tests/test_workflow.py
@@ -10,5 +10,20 @@ def test_init_beta_sim_wf(base_path, tr, tp,
 
     wf.inputs.input_node.out_dir = str(base_path)
     wf.inputs.input_node.fname = fname
+    wf.config['execution']['crashfile_format'] = 'txt'
+    wf.base_dir = base_path
+    wf.run()
 
+
+def test_simple_init_beta_sim_wf(base_path, tr, tp,
+                                 config_dict_simple):
+    fname = 'test.tsv'
+
+    wf = init_beta_sim_wf(config=config_dict_simple,
+                          n_simulations=10)
+
+    wf.inputs.input_node.out_dir = str(base_path)
+    wf.inputs.input_node.fname = fname
+    wf.config['execution']['crashfile_format'] = 'txt'
+    wf.base_dir = base_path
     wf.run()
