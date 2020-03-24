e5bdbc77c0b4d5042a19ea43a1f3a91e194db663
James Kent <james-kent@uiowa.edu>
2020-03-24 12:17:30 -0500

    add more draft code for simulated data

--- analysis/analysis_tst.py
+++ analysis/analysis_tst.py
@@ -1,6 +1,7 @@
 
 #%%
 import os
+from textwrap import dedent
 import seaborn as sns
 import pandas as pd
 import numpy as np
@@ -10,16 +11,17 @@ from scipy.stats import ttest_ind, ttest_1samp
 sns.set(style="ticks", rc={"lines.linewidth": 3.0})
 sns.set(font_scale=1.3)
 sns.set_style({'axes.grid': False})
-plt.style.use("analysis/gray_background.mplstyle")
+# plt.style.use("analysis/gray_background.mplstyle")
 
 #%%
-df = pd.read_csv('data/bulk_simulation32.tsv', sep='\t')
+df = pd.read_csv('../simulation_100_lsa_params_more.tsv', sep='\t')
 
 #%%
 df.loc[df['correlation_observed'] == 1, 'correlation_observed'] -= 0.0001
 df.loc[df['correlation_observed'] == -1, 'correlation_observed'] += 0.0001
 df['corr_obs_trans'] = np.arctanh(df['correlation_observed'])
 df['corr_obs_trans_clip'] = np.clip(df['corr_obs_trans'], -1.5, 1.5)
+df['trial_noise_group'] = np.rint(np.abs(np.log10((df['trial_noise_ratio'] / df['signal_magnitude'])))).astype(int)
 #%%
 # Draw a categorical scatterplot to show each observation
 g = sns.violinplot(x="correlation_target", y="corr_obs_trans",
@@ -54,75 +56,81 @@ snrs = df["signal_magnitude"].unique()
 #%%
 
 for snr in snrs:
-    df_tmp = df[df["signal_magnitude"] == snr]
-    g2 = sns.catplot(x="correlation_target", y="corr_obs_trans_clip",
-                     hue="estimation_method", col="iti_mean", row="n_trials",
-                     data=df_tmp, kind="violin", cut=0,
-                     )
-    g2.fig.suptitle("CNR: {}".format(snr))
-    r_targets = [-0.8, 0.0, 0.2, 0.4, 0.6, 0.8]
-    z_targets = [np.arctanh(r) for r in r_targets]
-    for sg in g2.axes.ravel():
-        add_targets(sg, targets=z_targets)
+    for trial_noise in df['trial_noise_group'].unique():
+        df_tmp = df.query(f'(signal_magnitude == {snr}) & (trial_noise_group == {trial_noise})')
+        g2 = sns.catplot(x="correlation_target", y="corr_obs_trans_clip",
+                         hue="estimation_method", col="iti_mean", row="n_trials",
+                         data=df_tmp, kind="violin", cut=0,
+                        )
+        g2.fig.suptitle("CNR: {}, trial_var: {}".format(snr, trial_noise))
+        r_targets = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+        z_targets = [np.arctanh(r) for r in r_targets]
+        for sg in g2.axes.ravel():
+            add_targets(sg, targets=z_targets)
 
-    g2.savefig("./outputs/snr-{snr}_simulations.svg".format(snr=snr))
+    g2.savefig("../outputs/snr-{snr}_trialvar-{tvar}_simulations.svg".format(snr=snr, tvar=trial_noise))
 
 
 #%%
 # seminar graph
-# graph 37.06 CNR
-snr = snrs[1]
 
-# sns.set_style({'axes.grid': False})
-query = ('(iti_mean == 2.0 | iti_mean == 8.0)'
-         ' & (n_trials == 15 | n_trials == 60)'
-         ' & (correlation_target == 0.2 | correlation_target == 0.4'
-         ' | correlation_target == 0.6 | correlation_target == 0.8)'
-         ' & (signal_magnitude == {snr})'.format(snr=snr))
-df_tmp = df.query(query)
-tmp_rename = {"correlation_target": "Ground Truth Correlation",
-              "corr_obs_trans_clip": "Observed Correlation (r-z)",
-              "n_trials": "# Trials",
-              "iti_mean": "Inter Trial Interval",
-              "estimation_method": "Estimation Method"}
-df_tmp.rename(columns=tmp_rename, inplace=True)
-g2 = sns.catplot(x="Ground Truth Correlation", y="Observed Correlation (r-z)",
-                 hue="Estimation Method",
-                 col="Inter Trial Interval", row="# Trials",
-                 data=df_tmp, kind="violin", cut=0,
-                 legend=False, margin_titles=True)
+for snr in snrs:
+    for trial_noise in df['trial_noise_group'].unique():
+        # sns.set_style({'axes.grid': False})
+        query = ('(iti_mean == 2.0 | iti_mean == 8.0)'
+                ' & (n_trials == 15 | n_trials == 60)'
+                ' & (correlation_target == 0.2 | correlation_target == 0.4'
+                ' | correlation_target == 0.6 | correlation_target == 0.8)'
+                ' & (signal_magnitude == {snr})'
+                ' & (trial_noise_group == {tng})'.format(snr=snr,
+                                                        tng=trial_noise))
+        df_tmp = df.query(query)
+        tmp_rename = {"correlation_target": "Ground Truth Correlation",
+                    "corr_obs_trans_clip": "Observed Correlation (r-z)",
+                    "n_trials": "# Trials",
+                    "iti_mean": "Inter Trial Interval",
+                    "estimation_method": "Estimation Method"}
+        df_tmp.rename(columns=tmp_rename, inplace=True)
+        g2 = sns.catplot(x="Ground Truth Correlation", y="Observed Correlation (r-z)",
+                        hue="Estimation Method",
+                        col="Inter Trial Interval", row="# Trials",
+                        data=df_tmp, kind="violin", cut=0,
+                        legend=False, margin_titles=True)
 
-r_targets = [0.2, 0.4, 0.6, 0.8]
-z_targets = [np.arctanh(r) for r in r_targets]
-for sg in g2.axes.ravel():
-    y_lab = sg.get_ylabel()
-    if y_lab:
-        sg.set_ylabel(y_lab, fontdict={'weight': 'heavy'})
-    x_lab = sg.get_xlabel()
-    if x_lab:
-        sg.set_xlabel(x_lab, fontdict={'weight': 'heavy'})
+        g2.fig.suptitle("CNR: {}, Trial Variance: {}".format(snr, trial_noise), y=1.02)
 
-    add_targets(sg, targets=z_targets, width=0.8)
+        r_targets = [0.2, 0.4, 0.6, 0.8]
+        z_targets = [np.arctanh(r) for r in r_targets]
+        for sg in g2.axes.ravel():
+            y_lab = sg.get_ylabel()
+            if y_lab:
+                sg.set_ylabel(y_lab, fontdict={'weight': 'heavy'})
+            x_lab = sg.get_xlabel()
+            if x_lab:
+                sg.set_xlabel(x_lab, fontdict={'weight': 'heavy'})
 
-# add legend
-lgnd_sg = g2.axes[0, 1]
-lgnd_sg.legend(title="Estimation Method",
-               loc="lower left")
+            add_targets(sg, targets=z_targets, width=0.8)
 
-g2.savefig('outputs/simplified_simulation.png', dpi=600)
-g2.savefig('outputs/simplified_simulation.svg')
+        # add legend
+        lgnd_sg = g2.axes[0, 1]
+        lgnd_sg.legend(title="Estimation Method",
+                    loc="lower left")
+
+        #g2.savefig('../outputs/snr-{snr}_trial_noise-{tng}_simplified_simulation.png', dpi=600)
+        g2.savefig('../outputs/snr-{snr}_trial_noise-{tng}_simplified_simulation.svg'.format(snr=snr, tng=trial_noise))
 
 #%%
 # test the null (appears to hold up within nominal error rates)
 short_lsa_query = ('(estimation_method == "lsa") &'
-                   '(iti_mean == 2.0) &'
-                   '(n_trials == 15) &'
-                   '(correlation_target == 0.4) &'
-                   '(signal_magnitude == 37.06)')
+                   '(iti_mean == 4.0) &'
+                   '(n_trials == 30) &'
+                   '(correlation_target == 0.2) &'
+                   '(signal_magnitude == 1) &'
+                   '(trial_noise_group == 0)')
 df_short_lsa = df.query(short_lsa_query)
 test_collector = {"t": [], "p": []}
 for _ in range(10000):
-    sample = np.random.choice(df_short_lsa['corr_obs_trans'], size=(40, 2),
+    sample = np.random.choice(df_short_lsa['corr_obs_trans'], size=(30, 2),
                               replace=False)
     group1 = sample[:, 0]
     group2 = sample[:, 1]
@@ -138,40 +146,61 @@ sns.distplot(test_df["p"], bins=20, kde=False)
 df_short_lsa.query('iteration == 0')
 
 #%%
-def test_power(df, estimation_method="lss", iti_mean=6.0,
-               n_trials=30, correlation_tgt1=0.4, correlation_tgt2=0.6,
-               signal_magnitude=37.06, simulations=10000,
-               sample_size=40):
+def test_power(df, estimation_method="lss", iti_mean=4.0,
+               n_trials=30, correlation_tgt1=0.0, correlation_tgt2=0.3,
+               signal_magnitude=1, simulations=10000,
+               trial_type1=None, trial_type2=None,
+               sample_size=40, trial_var=1):
     base_query = ('(estimation_method == "{em}") &'
                   '(iti_mean == {iti}) &'
                   '(n_trials == {trl}) &'
-                  '(signal_magnitude == {snr})').format(
+                  '(signal_magnitude == {snr}) &'
+                  '(trial_noise_group == {tv})').format(
                     em=estimation_method,
                     iti=iti_mean,
                     trl=n_trials,
-                    snr=signal_magnitude
+                    snr=signal_magnitude,
+                    tv=trial_var,
                   )
     group1_query = base_query + \
         '& (correlation_target == {tgt})'.format(
             tgt=correlation_tgt1)
+    if trial_type1:
+        group1_query = group1_query + \
+            '& (trial_type == "{tp}")'.format(tp=trial_type1)
 
     group2_query = base_query + \
         '& (correlation_target == {tgt})'.format(
             tgt=correlation_tgt2)
 
+    if trial_type2:
+        group2_query = group2_query + \
+            '& (trial_type == "{tp}")'.format(tp=trial_type2)
+
     group1 = df.query(group1_query)
     group2 = df.query(group2_query)
-    group_diff = group1['corr_obs_trans'].values - group2['corr_obs_trans'].values
 
+    target_diff = abs(correlation_tgt2 - correlation_tgt1)
     test_collector = {"t_value": [], "p_value": [],
+                      "tgt_corr_diff": [target_diff] * simulations,
+                      "trial_var": [trial_var] * simulations,
                       "estimation_method": [estimation_method] * simulations,
                       "iti_mean": [iti_mean] * simulations,
                       "n_trials": [n_trials] * simulations,
                       "signal_magnitude": [signal_magnitude] * simulations}
 
     for _ in range(simulations):
-        sample = np.random.choice(group_diff, size=sample_size,
-                                  replace=False)
+        if correlation_tgt1 == correlation_tgt2:
+            overall_sample = np.random.choice(
+                group1['corr_obs_trans'].values,
+                int(sample_size*2), replace=False)
+            group1_sample = overall_sample[0:sample_size]
+            group2_sample = overall_sample[sample_size:]
+        else:
+            group1_sample = np.random.choice(group1['corr_obs_trans'].values, sample_size, replace=False)
+            group2_sample = np.random.choice(group2['corr_obs_trans'].values, sample_size, replace=False)
+        sample = group1_sample - group2_sample
+
         t, p = ttest_1samp(sample, 0)
         test_collector["t_value"].append(t)
         if correlation_tgt1 < correlation_tgt2 and t > 0 and p < 0.05:
@@ -188,19 +217,93 @@ def test_power(df, estimation_method="lss", iti_mean=6.0,
 
 
 #%%
+# test the baseline assumption that no difference correlations
+# should have a 5% false positive rate
 stat_collector = []
 pwr_collector = {}
 for est in ["lsa", "lss"]:
-    for iti_mean in [2.0, 8.0]:
+    for iti_mean in [2.0, 4.0, 8.0]:
         for n_trials in [15, 60]:
-            test_df, pwr = test_power(
-                df, estimation_method=est, n_trials=n_trials,
-                iti_mean=iti_mean)
-            stat_collector.append(test_df)
-            pwr_collector[(est, iti_mean, n_trials)] = pwr
+            for signal_magnitude in [1, 10]:
+                for trial_noise in [0, 1]:
+                    pwr_key = (est, iti_mean, n_trials, signal_magnitude, trial_noise)
+                    pwr_collector[pwr_key] = []
+                    for tgt_corr in [(0.0, 0.3),
+                                    (0.1, 0.4),
+                                    (0.2, 0.5),
+                                    (0.3, 0.6),
+                                    (0.4, 0.7),
+                                    (0.5, 0.8),
+                                    (0.6, 0.9)]:
+                        test_df, pwr = test_power(
+                            df, estimation_method=est, n_trials=n_trials,
+                            iti_mean=iti_mean, signal_magnitude=signal_magnitude,
+                            correlation_tgt1=tgt_corr[0], correlation_tgt2=tgt_corr[1],
+                            simulations=1429, sample_size=50, trial_var=trial_noise)
+                        stat_collector.append(test_df)
+                        pwr_collector[pwr_key].append(pwr)
 #%%
 df_pwr = pd.concat(stat_collector)
 
+
+#%%
+tmp_rename['p_value'] = "p value"
+df_pwr_tmp = df_pwr.rename(columns=tmp_rename)
+for signal_magnitude in [1, 10]:
+    for trial_var in [0, 1]:
+        df_pwr_select = df_pwr_tmp.query(
+            (
+                '(signal_magnitude == {snr}) &'
+                '(trial_var == {tv})'
+            ).format(snr=signal_magnitude, tv=trial_var)
+        )
+        g_fac = sns.FacetGrid(df_pwr_select, row="# Trials", col="Inter Trial Interval",
+                            hue="Estimation Method",
+                            hue_order=["lss", "lsa"], margin_titles=True,
+                            sharey=False)
+
+        g_hist = (g_fac.map(sns.distplot, "p value", hist=True, bins=20))
+        sg_hist_lgnd = g_hist.axes[0, 1]
+        sg_hist_lgnd.legend()
+
+        txtstr = dedent(r"""lss pwr: {lss_pwr}
+lsa pwr: {lsa_pwr}""")
+
+        g_fac.fig.suptitle("CNR: {}, Trial Variance: {}".format(signal_magnitude, trial_var), y=1.02)
+
+        props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+        for iti_mean, ax, n_trials in zip([2.0, 4.0, 8.0, 2.0, 4.0, 8.0],
+                                        g_hist.axes.ravel(),
+                                        [15, 15, 15, 60, 60, 60]):
+            ax.set_yticklabels([])
+            lss_pwr = np.array(pwr_collector[("lss", iti_mean, n_trials, signal_magnitude, trial_var)]).mean()
+            lsa_pwr = np.array(pwr_collector[("lsa", iti_mean, n_trials, signal_magnitude, trial_var)]).mean()
+
+            txtrepl = txtstr.format(lss_pwr=np.round(lss_pwr, 2),
+                                    lsa_pwr=np.round(lsa_pwr, 2))
+            ax.text(0.25, 0.45, txtrepl, transform=ax.transAxes, fontsize=14,
+                    verticalalignment='center',
+                    horizontalalignment="left", bbox=props)
+        # g_hist.savefig('../outputs/snr-{}_trial_noise-{}_simplified_pwr.png', dpi=400)
+        g_hist.savefig('../outputs/snr-{}_trial_noise-{}_diff-large_simplified_pwr.svg'.format(signal_magnitude, trial_var))
+
+#%%
+stat_collector = []
+pwr_collector = {}
+for est in ["lsa", "lss"]:
+    for iti_mean in [2.0, 4.0]:
+        for n_trials in [15, 60]:
+            for tgt_corr in [(0.0, 0.1), (0.1, 0.2), (0.2, 0.3)]:
+                test_df, pwr = test_power(
+                    df, estimation_method=est, n_trials=n_trials,
+                    iti_mean=iti_mean, signal_magnitude=10,
+                    correlation_tgt1=tgt_corr[0], correlation_tgt2=tgt_corr[1])
+                stat_collector.append(test_df)
+                pwr_collector[(est, iti_mean, n_trials)] = pwr
+
+
+#%%
+df_pwr = pd.concat(stat_collector)
 
 #%%
 tmp_rename['p_value'] = "p value"
@@ -218,9 +321,8 @@ sg_hist_lgnd.legend()
 txtstr = r"""lss pwr: {lss_pwr}
 lsa pwr: {lsa_pwr}"""
 
-
 props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-for iti_mean, ax, n_trials in zip([2.0, 8.0, 2.0, 8.0],
+for iti_mean, ax, n_trials in zip([2.0, 4.0, 2.0, 4.0],
                                   g_hist.axes.ravel(),
                                   [15, 15, 60, 60]):
     lss_pwr = pwr_collector[("lss", iti_mean, n_trials)]
@@ -231,9 +333,49 @@ for iti_mean, ax, n_trials in zip([2.0, 8.0, 2.0, 8.0],
     ax.text(0.25, 0.45, txtrepl, transform=ax.transAxes, fontsize=14,
             verticalalignment='center',
             horizontalalignment="left", bbox=props)
-g_hist.savefig('outputs/simplified_pwr.png', dpi=400)
-g_hist.savefig('outputs/simplified_pwr.svg')
+#%%
+bold_pwr_dict = {"method": [], "power": [], "participants": []}
+for method in ["lsa", "lss"]:
+    # from 5 participants to 40
+    for participants in range(5, 60):
+        bold_df, bold_pwr = test_power(
+            df,
+            estimation_method=method,
+            iti_mean=4.0,
+            n_trials=60,
+            signal_magnitude=10,
+            correlation_tgt1=0.0,
+            correlation_tgt2=0.3,
+            sample_size=participants)
+        bold_pwr_dict['method'].append(method)
+        bold_pwr_dict['participants'].append(participants)
+        bold_pwr_dict['power'].append(bold_pwr)
 
+bold_pwr_df = pd.DataFrame.from_dict(bold_pwr_dict)
+bold_pwr_df.head()
+
+#%%
+pp = sns.lineplot(
+    x='participants',
+    y='power',
+    hue='method',
+    legend="brief",
+    hue_order=["lss", "lsa"],
+    data=bold_pwr_df)
+
+lgnd = pp.legend()
+lgnd.get_texts()[0].set_text('')
+for txt in lgnd.get_texts()[1:]:
+    txt.set_fontsize(15)
+lgnd.set_title("Estimation Method", prop={'size': 15, 'weight': 'heavy'})
+pp.set_xlabel("# of Participants",
+              fontdict={'fontsize': 15, 'fontweight': 'heavy'})
+pp.set_ylabel("Power",
+              fontdict={'fontsize': 15, 'fontweight': 'heavy'})
+pp.axhline(0.8, linestyle='--')
+plt.tight_layout()
+pp.figure.savefig('../outputs/power_plot.svg')
+pp.figure.savefig('../outputs/power_plot.png', dpi=400)
 # TEST ON REAL DATA
 ## get the events.tsv formatted correctly
 #%%
@@ -385,4 +527,19 @@ pp.axhline(0.8, linestyle='--')
 plt.tight_layout()
 pp.figure.savefig('outputs/power_plot.svg')
 pp.figure.savefig('outputs/power_plot.png', dpi=400)
+
 #%%
+stat_collector = []
+pwr_collector = {}
+for est in ["lsa", "lss"]:
+    test_df, pwr = test_power(
+                    df, estimation_method=est, n_trials=50,
+                    iti_mean=18.84, signal_magnitude=1,
+                    correlation_tgt1=0.1, correlation_tgt2=0.4,
+                    trial_type1="repeat", trial_type2="switch",
+                    sample_size=30, trial_var=1)
+    stat_collector.append(test_df)
+    pwr_collector[est] = pwr
+
+
+# %%
